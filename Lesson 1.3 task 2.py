cbc6d78770d941b712b102197e35f7c04af29a58
Ivan <1>
2023-09-04 17:46:31 +0500

    added task 2

---
+++ Lesson 1.3 task 2.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+
+
+def count(n, k):
+    if k == 0:
+        return 1
+    if k > n:
+        return 0
+    else:
+        return count(n-1, k) + count(n-1, k-1)
+
+
+def main():
+    print(count(n,k))
+
+
+if __name__ == "__main__":
+    main()
