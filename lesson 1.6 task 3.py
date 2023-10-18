c4a5f773f59619405fd846fbff76daf01a240129
Ivan <1>
2023-10-18 18:47:29 +0500

    Added 1.6 task 1

---
+++ lesson 1.6 task 3.py
@@ -0,0 +1,14 @@
+import time
+
+
+class Loggable:
+
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+
+
+class LoggableList(list, Loggable):
+
+    def append(self, elem):
+        super().append(elem)
+        return self.log(elem)
