090eca14bda7766c24ebb32c2ded0b3ce56637b6
Ivan <1>
2023-10-27 17:43:27 +0500

    Added 1.6 task 1

---
+++ Lesson 2.1 task 1.py
@@ -0,0 +1,9 @@
+try:
+    foo()
+except AssertionError:
+    print('AssertionError')
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ArithmeticError:
+    print('ArithmeticError')
+
