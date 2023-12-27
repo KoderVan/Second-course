c9d668ec9b3880e897f32b9d159c97dcd397ee8f
Ivan <1>
2023-12-27 16:00:43 +0500

    functions modified

--- Lesson 3.3 task 1.py
+++ Lesson 3.3 task 1.py
@@ -1,19 +1,30 @@
 import re
-
 import requests
 
 
-first_link = input()
-second_link = input()
-request_link = requests.get(first_link).text
-pattern = r"href=\"([\w\:\/\.]+)"
-search = re.findall(pattern, request_link)
-inner_links = []
-for link in search:
-    get_link = requests.get(link).text.strip()
-    inner_links += re.findall(pattern, get_link.replace('stepic.org', 'stepik.org'))
+def get_links(first_link):
+    request_link = requests.get(first_link).text
+    pattern = r"href=\"([\w\:\/\.]+)"
+    search = re.findall(pattern, request_link)
+    inner_links = []
+    for link in search:
+        get_link = requests.get(link).text.strip()
+        inner_links += re.findall(pattern, get_link.replace('stepic.org', 'stepik.org'))
+    return inner_links
 
-if second_link in inner_links:
-    print('Yes')
-else:
-    print('No')
+
+def result_of_search(list_of_links, second_link):
+    if second_link in list_of_links:
+        return 'Yes'
+    else:
+        return 'No'
+
+
+def main():
+    list_of_links = get_links(input())
+    final_result = result_of_search(list_of_links, input())
+    print(final_result)
+
+
+if __name__ == "__main__":
+    main()
